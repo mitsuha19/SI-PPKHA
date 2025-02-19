c75daf6b176d2860a39c893988c93908d50faa04
Rohit Panjaitan <rohitpanjaitan7@gmail.com>
2025-02-19 09:21:48 +0700

    Initial Project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SI-PPKHA</title>
+    <link href="" />
+</head>
+
+<body>
+    <div class="container-fluid p-5">
+        @yield('content')
+    </div>
+    <script src=""></script>
+</body>
+
+</html>
