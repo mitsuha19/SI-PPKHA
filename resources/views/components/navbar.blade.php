cd19bb1cf250175697702785bc241930ca3b0cdc
Prans Daniel <pransdaniel04@gmail.com>
2025-02-20 08:05:55 +0700

    update navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,16 +3,16 @@
         <div class="container d-flex justify-content-between align-items-center">
             <!-- Logo dan Judul -->
             <a class="d-flex align-items-center text-white text-decoration-none">
-                <img src="{{ asset('assets/images/itdel.jpg') }}" alt="Logo" width="40" height="auto" class="me-3">
-                <div class="d-flex flex-column" style="line-height: 0.9; gap: 0px;">
+                <img src="{{ asset('assets/images/itdel.jpg') }}" alt="Logo" width="40" height="auto" class="me-3"style="margin-right: 10px;">
+                <div class="d-flex flex-column align-items-center text-center" style="line-height: 0.9; gap: 0px;">
                     <span class="fs-4 fw-bold">CAIS</span>
-                    <p class="m-0 career-text" style="font-size: 12px; margin-top: -4px;">Career Alumni Information System</p>
+                    <p class="m-0 career-text two-line-text" style="font-size: 12px; margin-top: -4px;">Career Alumni Information System</p>
                 </div>
             </a>
         </div>
        
         <!-- Menu Navigasi -->
-        <ul class="flex space-x-6 text-white font-medium">
+        <ul class="flex space-x-6 text-white ">
             <li><a href="/" class="hover:underline">Beranda</a></li>
             <li><a href="/berita" class="hover:underline">Berita</a></li>
             <li><a href="/pengumuman" class="hover:underline">Pengumuman</a></li>
