cfcd0d55639cd8f46f3b58df4e60d801a2e02b9e
Ruth <ruth.siregar10@gmail.com>
2025-02-24 05:44:40 +0700

    page berita (admin)

--- resources/views/admin/berita.blade.php
+++ resources/views/admin/berita.blade.php
@@ -1,8 +1,112 @@
-@extends('layouts.app')
+@extends('layouts.appAdmin')
 
 @section('content')
 @include('components.navbarAdmin')
-<div class="main-content">
-  <h1 >Ini Berita</h1>
+<div class="main-content d-flex flex-column justify-content-center align-items-center">
+  <h1>Berita</h1>
+
+  <div class="d-flex flex-row gap-2 mb-4">
+    <form>
+      <input type="text" id="berita" name="berita" value="Cari Berita">
+    </form>
+    <div class="search-logo d-flex justify-content-center align-items-center" >
+      <i class='bx bx-search-alt-2'></i>
+    </div>
+  </div>
+
+  <div class="background-card">
+    <div class="card-berita d-flex align-items-center px-3">
+      <img style="width: 151px" src="{{ asset('assets/images/image.png') }}">
+      <div class="ps-3">
+        {{-- Judul Berita dan button Edit dan Delete --}}
+        <div class="d-flex flex-row w-auto justify-content-start align-items-enc">
+          <h2 class="fst-italic roboto-title mb-0 align-self-center" style="margin-right: 90px">Ratusan Mahasiswa Indonesia Terima Mahasiswa </h2>
+  
+          {{-- Button --}}
+          <div class="align-self-start">
+            <button type="button" class="btn">
+              <i class='bx bx-pencil'></i>
+              Edit
+            </button>
+            <button type="button" class="btn">
+              <i class='bx bx-trash' ></i>
+              Hapus
+            </button>
+          </div>
+        </div>
+
+        <hr class="my-2" style="border: 3px solid black;">
+  
+        <p class="roboto-light mb-1 mt-2" style="font-size: 15px">
+          204 Mahasiswa Menerima Beasiswa TELADAN. Beasiswa ini untuk menciptakan pemimpin-pemimpin masa depan yang dibekali dengan soft skills..
+        </p>
+  
+        {{-- Link to detail news --}}
+        <div class="detail">
+          <a href="">
+            Selengkapnya..
+          </a>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+
+  <div class="background-card">
+    <div class="card-berita d-flex align-items-center px-3">
+      <img style="width: 151px" src="{{ asset('assets/images/image.png') }}">
+      <div class="ps-3">
+        {{-- Judul Berita dan button Edit dan Delete --}}
+        <div class="d-flex flex-row w-auto justify-content-start align-items-enc">
+          <h2 class="fst-italic roboto-title mb-0 align-self-center" style="margin-right: 90px">Ratusan Mahasiswa Indonesia Terima Mahasiswa </h2>
+  
+          {{-- Button --}}
+          <div class="align-self-start">
+            <button type="button" class="btn">
+              <i class='bx bx-pencil'></i>
+              Edit
+            </button>
+            <button type="button" class="btn">
+              <i class='bx bx-trash' ></i>
+              Hapus
+            </button>
+          </div>
+        </div>
+
+        <hr class="my-2" style="border: 3px solid black;">
+  
+        <p class="roboto-light mb-1 mt-2" style="font-size: 15px">
+          204 Mahasiswa Menerima Beasiswa TELADAN. Beasiswa ini untuk menciptakan pemimpin-pemimpin masa depan yang dibekali dengan soft skills..
+        </p>
+  
+        {{-- Link to detail news --}}
+        <div class="detail">
+          <a href="">
+            Selengkapnya..
+          </a>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+  <div class="pagination">
+    <a href="#" style="background-color: transparent">&laquo;</a>
+    <a class="active" href="#">1</a>
+    <a href="#">2</a>
+    <a href="#">3</a>
+    <a href="#">4</a>
+    <a href="#">...</a>
+    <a href="#" style="background-color: transparent">&raquo;</a>
+  </div>
+
+  <div class="align-self-end justify-self-end">
+    <button type="button" class="btn">
+      <i class='bx bx-plus-circle' ></i>
+      Tambah
+    </button>
+  </div>
+  
 </div>
 @endsection
