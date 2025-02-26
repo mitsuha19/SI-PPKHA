e83aa085d9ee1c6c3835c555106654da484905e0
xyez <kitaanakindo@gmail.com>
2025-02-26 11:41:14 +0700

    CAIS fixed - all Pages
    Update: Can reused bg.blade.php

--- resources/views/components/bg.blade.php
+++ resources/views/components/bg.blade.php
@@ -1,18 +1,31 @@
-<div class="wave wave-top">
-  <svg viewBox="0 0 1440 320">
-      <path fill="#7fc1e3" fill-opacity="2"
-          d="M0,64L48,85.3C96,107,192,149,288,149.3C384,149,480,107,576,85.3C672,64,768,64,864,80C960,96,1056,128,1152,144C1248,160,1344,160,1392,160L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
-      </path>
-  </svg>
-</div>
+<div class="background-container">
+  <div class="wave wave-top">
+      <svg viewBox="0 150 1440 671">
+          <defs>
+              <linearGradient id="waveGradientTop" x1="0" y1="0" x2="0" y2="1" gradientUnits="objectBoundingBox">
+                  <stop offset="10%" stop-color="#91D3E6"/>
+                  <stop offset="60%" stop-color="#6FAEC7"/>
+                  <stop offset="100%" stop-color="#436973"/>
+              </linearGradient>
+          </defs>
+          <path fill="url(#waveGradientTop)" fill-opacity="1"  /* Corrected opacity to 1 (valid range is 0 to 1) */
+              d="M-1 0H1439V303.04V606.08C1439 606.08 1209 506.379 1054 493.697C899 481.016 719 606.08 719 606.08C719 606.08 527.5 720.65 359 606.08C190.5 491.511 -1 606.08 -1 606.08V0Z">
+          </path>
+      </svg>
+  </div>
 
-
-
-<!-- Gelombang Bawah -->
-<div class="wave wave-bottom">
-  <svg viewBox="0 0 1440 320">
-      <path fill="#7fc1e3" fill-opacity="1"
-          d="M0,224L48,213.3C96,203,192,181,288,197.3C384,213,480,267,576,256C672,245,768,171,864,133.3C960,96,1056,96,1152,122.7C1248,149,1344,203,1392,229.3L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
-      </path>
-  </svg>
+  <div class="wave wave-bottom">
+      <svg viewBox="0 0 1440 400">
+          <defs>
+              <linearGradient id="waveGradientBottom" x1="0" y1="0" x2="0" y2="1" gradientUnits="objectBoundingBox">
+                  <stop offset="0%" stop-color="#436973"/>
+                  <stop offset="50%" stop-color="#7DC4D6"/>
+                  <stop offset="100%" stop-color="#8ddaed"/>
+              </linearGradient>
+          </defs>
+          <path fill="url(#waveGradientBottom)" fill-opacity="1"
+          d="M1440 657H0V353.96V50.9197C0 50.9197 230 150.621 385 163.303C540 175.984 720 50.9197 720 50.9197C720 50.9197 911.5 -63.6496 1080 50.9197C1248.5 165.489 1440 50.9197 1440 50.9197V657Z">
+          </path>
+      </svg>
+  </div>
 </div>
--- resources/views/ppkha/tentang.blade.php
+++ resources/views/ppkha/tentang.blade.php
@@ -1,7 +1,73 @@
 @extends('layouts.app')
 
 @section('content')
-@include('components.navbar')
-@include('components.bg')
-@include('components.footer')
+    @include('components.navbar')
+
+    <div class="content-with-background">
+        @include('components.bg') <!-- Renders the background waves -->
+
+        <!-- Berita Section -->
+      <div class="pengumuman-section">
+        <h2 class="section-title">BERITA</h2>
+        <div class="pengumuman-grid">
+            <!-- Static Cards -->
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 1">
+                <div class="card-body">
+                    <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 2">
+                <div class="card-body">
+                    <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 3">
+                <div class="card-body">
+                    <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                    <a href="#" class="btn btn-primary">Selengkapnya</a>
+                </div>
+            </div>
+        </div>
+      </div>
+
+      <!-- Berita Section -->
+    <div class="pengumuman-section">
+      <h2 class="section-title">BERITA</h2>
+      <div class="pengumuman-grid">
+          <!-- Static Cards -->
+          <div class="card" style="width: 18rem;">
+              <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 1">
+              <div class="card-body">
+                  <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                  <a href="#" class="btn btn-primary">Selengkapnya</a>
+              </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+              <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 2">
+              <div class="card-body">
+                  <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                  <a href="#" class="btn btn-primary">Selengkapnya</a>
+              </div>
+          </div>
+
+          <div class="card" style="width: 18rem;">
+              <img src="{{ asset('assets/images/image.png') }}" class="card-img-top" alt="Pengumuman 3">
+              <div class="card-body">
+                  <h5 class="card-title">IT Del Akan Mengadakan KMC (Keluarga Mahasiswa Cup)</h5>
+                  <a href="#" class="btn btn-primary">Selengkapnya</a>
+              </div>
+          </div>
+      </div>
+  </div>
+    </div>
+
+    @include('components.footer')
 @endsection
