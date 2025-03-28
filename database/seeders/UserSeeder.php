1d822c153ca5cd7a1f5f4043600badc78ca4a136
zzzXenon <royjonathan03082003@gmail.com>
2025-03-28 16:24:40 +0700

    Update UserSeeder.php
    lontong sayur

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
     $alumniRole = Role::firstOrCreate(['name' => 'alumni']);
 
     // Retrieve Fakultas records
-    $fakultasFite = Fakultas::where('name', 'FITE')->first();
+    $fakultasFite = Fakultas::where('name', 'Fakultas Informatika dan Teknik Elektro')->first();
     $fakultasVokasi = Fakultas::where('name', 'Vokasi')->first();
 
     // Retrieve Prodi records
